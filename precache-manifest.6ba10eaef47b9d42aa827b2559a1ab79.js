8f72488933d271588bc36620f7da303bc03a05aa
mikhail.s.korovin <96950629+mikhailkorovin1991@users.noreply.github.com>
2023-04-08 21:50:40 +0300

    Add files via upload

---
+++ precache-manifest.6ba10eaef47b9d42aa827b2559a1ab79.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d38df10541bd23192d1bc3c3dcd4343",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c52573744ac9c17f0306",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "f8cda03a0698feb3be5b",
+    "url": "/static/js/2.612d2c84.chunk.js"
+  },
+  {
+    "revision": "c52573744ac9c17f0306",
+    "url": "/static/js/main.cc681eba.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
